5ec298aa9a77ae3751bad6cfcf76955fb3664222
Vanshika <vanshikaaa1009@gmail.com>
2020-10-20 03:37:27 +0530

    Add files via upload

---
+++ src/main/java/sonar/Hello.java
@@ -0,0 +1,11 @@
+package sonar;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, We are here SonarQube!!!");
+
+	}
+
+}
